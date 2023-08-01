3b28816cd6c942c1a23db46158356a02348d70de
VitolLub <vitollub@gmail.com>
2023-08-01 17:26:05 +0300

    Parse rieltors

---
+++ rieltors_scrap.py
@@ -0,0 +1,23 @@
+import requests
+
+class Rieltors:
+    def __init__(self):
+        self.domain = ''
+
+    def make_request(self):
+        pass
+
+    def save_data(self):
+        pass
+
+    def get_data(self):
+        pass
+
+    def parse_data(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    rieltors = Rieltors()
+    rieltors.domain 
